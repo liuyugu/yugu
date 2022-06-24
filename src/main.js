5d16836299b0b661bffef34a453c8dc1e58e78b8
LYG <lyugu@qq.com>
2022-06-24 16:18:30 +0800

    import axios in main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/commons.css'
 import './router/promission'
+import axios from 'axios'
 import store from '@/store/index'  //引入store/index
 
 Vue.config.productionTip = false
-
+Vue.prototype.$axios = axios    //在Vue的原型上添加axios对象。
 
 new Vue({
   render: h => h(App),
